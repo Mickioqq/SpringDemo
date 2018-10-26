8cd6e3fa40387ead6c6e1b01dc2a8b381d681edf
qqwen <qqwen@Ctrip.com>
2018-10-26 13:46:07 +0800

    aop 前置通知

--- src/main/java/com/wqq/spring/controller/spring_aop/before/LoggingAspect.java
+++ src/main/java/com/wqq/spring/controller/spring_aop/before/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.wqq.spring.controller.spring_aop.before;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,8 @@ class LoggingAspect {
     //声明该方法为一个前置通知:目标方法开始之前执行
     //匹配该类下面所有的方法 *
     //@Before("execution(public void com.wqq.spring.controller.spring_aop.before.UserDao.*(String))")
+    //*匹配
+    //@Before("execution(* com.wqq.spring.controller.spring_aop.before.*.*(String))")
 
     @Before("execution(public void com.wqq.spring.controller.spring_aop.before.UserDao.save(String))")
     public void BeforeMethod(JoinPoint joinPoint){
@@ -24,4 +27,20 @@ class LoggingAspect {
         List<Object> arges= Arrays.asList(joinPoint.getArgs());
         System.out.println("The Method " + methodName +  " begins with" + arges);
     }
+
+    /**
+     * 后置通知:在目标方法执行后(无论是否发生异常),执行通知
+     * 在后置通知里还不能访问目标方法执行的结果
+     * @param joinPoint
+     */
+    @After("execution(* com.wqq.spring.controller.spring_aop.before.*.save(String))")
+    public void AfterMethod(JoinPoint joinPoint){
+
+        //目标方法名
+        String methodName=joinPoint.getSignature().getName();
+        //目标方法参数
+        List<Object> arges= Arrays.asList(joinPoint.getArgs());
+        System.out.println("The Method " + methodName +  " ends" + arges);
+    }
+
 }
